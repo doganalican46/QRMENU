99c86bf130dd61972ab257555e57772590043c3b
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-11-12 17:54:49 +0300

    Add project files.

---
+++ QRMENU/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRMENU.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Urunler()
+        {
+            return View();
+        }
+
+        public ActionResult Kategoriler()
+        {
+            return View();
+        }
+
+        public ActionResult Kampanyalar()
+        {
+            return View();
+        }
+
+        public ActionResult Profil()
+        {
+            return View();
+        }
+
+        public ActionResult Guvenlik()
+        {
+            return View();
+        }
+        public ActionResult Ayarlar()
+        {
+            return View();
+        }
+
+
+    }
+}
